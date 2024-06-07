981e2c8b33236c346f64f1fceb8a68d938a264a4
arianchhim978 <147645634+arianchhim978@users.noreply.github.com>
2024-06-07 01:40:56 -0400

    updated

---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+ 
+
+export default function Header(){
+    <div>
+        <ul>
+            <li><a href='index.html'>Home</a></li>
+            <li><a href='#'>About</a></li>
+        </ul>
+    </div>
+}
---
+++ render.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Header from './index'
+
+
+
+
+
+ReactDOM.render(
+    <div>
+        <Header/>
+    </div>,
+    document.getElementById('root')
+
+);
